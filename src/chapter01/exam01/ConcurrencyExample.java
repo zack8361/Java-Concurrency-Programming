56b6dbe4f80a1e214fc57607569c3e5debbe8486
zack8361 <71763096+zack8361@users.noreply.github.com>
2024-11-21 17:15:20 +0900

    1일차:동시성과 병렬성의 차이점

---
+++ src/chapter01/exam01/ConcurrencyExample.java
@@ -0,0 +1,36 @@
+package chapter01.exam01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConcurrencyExample {
+    public static void main(String[] args) {
+
+//        int cpuCores = Runtime.getRuntime().availableProcessors() * 2;
+
+        int cpuCores = 5;
+
+        List<Integer> data = new ArrayList<>();
+        for (int i = 0; i < cpuCores; i++) {
+            data.add(i);
+        }
+
+        //CPU 개수만큼 데이터를 병렬로 처리
+        long startTime = System.currentTimeMillis();
+
+        long sum = data.parallelStream()
+                .mapToLong(i -> {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return  i * i;
+                })
+                .sum();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("CPU 개수만큼 데이터를 병렬로 처리하는데 걸린 시간 : " + (endTime - startTime) + "ms");
+        System.out.println(sum);
+    }
+}
